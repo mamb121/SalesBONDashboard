95a81b17a8d3ab632caa4e32f32f4f08ba8d850c
mamb121 <mamb121@hotmail.com>
2024-03-06 01:15:14 +0300

    Add Filter Menu

---
+++ src/components/BranshFilter.vue
@@ -0,0 +1,34 @@
+<template>
+<v-navigation-drawer
+        location="right"
+        v-model="drawer"
+      >
+        <template v-slot:prepend>
+          <v-list-item
+            lines="two"
+            prepend-avatar="https://randomuser.me/api/portraits/women/81.jpg"
+            subtitle="Logged in"
+            title="Jane Smith"
+          ></v-list-item>
+        </template>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="height: 250px"></v-main>
+</template>
+<script lang="ts">
+import {Options,Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    drawer:Boolean
+  }
+})
+export default class BranshFilter extends Vue {
+    drawer!:boolean
+}
+</script>
--- src/views/TotalBranshSalesView.vue
+++ src/views/TotalBranshSalesView.vue
@@ -1,11 +1,34 @@
 <template>
-    <div id="myLineChart" style="width:100%; height:600px; margin: auto;"></div>
+  <v-card
+    class="mx-auto my-1"
+    elevation="1"
+  >
+    <v-card-item
+    style="border-bottom: 1px solid gray;"
+    >
+    <div class="px-0 d-flex" style="justify-content: space-between; align-items: center;">
+      <v-card-title
+      class="text-left"
+      >
+        Total Branch Sales
+      </v-card-title>
+      <v-btn icon class="text-right"  elevation="0" @click="!drawer">
+          <v-icon>mdi-filter</v-icon>
+      </v-btn>
+    </div>
+    </v-card-item>
+    <v-card-text>
+      <div id="myLineChart" style="width:100%; height:600px; margin: auto;"></div>
+    </v-card-text>
+  </v-card>
+  <BranshFilter :drawer="drawer"></BranshFilter>
 </template>
 
 <script lang="ts">
 import * as echarts from 'echarts/core';
 import { use } from 'echarts/core';
 import { CanvasRenderer } from 'echarts/renderers';
+import BranshFilter from '@/components/BranshFilter.vue'; // @ is an alias to /src
 
 import { BarChart,PieChart,LineChart } from 'echarts/charts';
 
@@ -13,6 +36,7 @@ import {
   TitleComponent,
   TooltipComponent,
   LegendComponent,
+
 } from 'echarts/components';
 
 import { GridComponent } from 'echarts/components';
@@ -29,6 +53,14 @@ use([
 ]);
 
 export default{
+  components:{
+    BranshFilter
+  },
+  data(){
+    return {
+      drawer:true
+  }
+  },
   mounted() {
     const pieOptions: echarts.EChartsCoreOption = {
       title: {
